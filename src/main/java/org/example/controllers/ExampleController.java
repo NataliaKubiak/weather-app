0c15222741f512c160556b4f1e9178ef06215bd0
Natalia Kubiak <nataliia.kubiak@gmail.com>
2025-01-14 14:38:34 +0100

    feat: add spring mvc config + first page with test controller

---
+++ src/main/java/org/example/controllers/ExampleController.java
@@ -0,0 +1,15 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ppp")
+public class ExampleController {
+
+    @GetMapping()
+    public String index() {
+        return "main/index";
+    }
+}
