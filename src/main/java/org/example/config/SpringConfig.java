c2ec4d5447eecc06c24baf99680c18aeb48e3644
Natalia Kubiak <nataliia.kubiak@gmail.com>
2025-01-17 11:36:38 +0100

    feat: add proper column names generation to hibernate config

--- src/main/java/org/example/config/SpringConfig.java
+++ src/main/java/org/example/config/SpringConfig.java
@@ -104,6 +104,8 @@ public class SpringConfig implements WebMvcConfigurer {
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
         properties.put("hibernate.hbm2ddl.auto", "none");
+        properties.put("hibernate.physical_naming_strategy",
+                "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
         return properties;
     }
 
