0c54bbbf2661e86b7f1d409b4846765eb61b0e48
Natalia Kubiak <nataliia.kubiak@gmail.com>
2025-01-30 21:35:04 +0100

    refactor: update password validation

--- src/main/java/org/example/entities/dto/NewUserDto.java
+++ src/main/java/org/example/entities/dto/NewUserDto.java
@@ -1,6 +1,7 @@
 package org.example.entities.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +17,8 @@ public class NewUserDto {
     @NotBlank(message = "Login is required")
     private String login;
 
-    @NotBlank(message = "Password is required")
     @Size(min = 4, message = "Password must be at least 4 characters long")
+    @Pattern(regexp = "^(?!.*\\s$).*", message = "Password cannot contain only spaces or end with space")
     private String password;
 
     private String repeatPassword;
